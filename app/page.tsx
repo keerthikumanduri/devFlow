7402d460854966ebf2d87ab5c44a1ebfe5a1cd7b
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-02-09 13:24:20 +0530

    Assets and MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-[#FF7000]">
-    Welcome the the world of Next.js
-  </h1>
+  <>
+    <h1 className="text-3xl font-black text-[#FF7000]">
+      Welcome the the world of Next.js
+    </h1>
+    <h1 className="font-space-grotesk text-3xl text-[#FF7000]">Jai Hanuman</h1>
+  </>
 );
 
 export default Home;
