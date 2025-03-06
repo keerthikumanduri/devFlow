329d9c429f9ab5c8e8d2923e0c9aa947100bfa44
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-03-06 10:31:05 +0530

    Create Navbar, Configured shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ const Home = () => (
     <h1 className="text-3xl font-black text-[#FF7000]">
       Welcome the the world of Next.js
     </h1>
-    <h1 className="font-space-grotesk text-3xl text-[#FF7000]">Jai Hanuman</h1>
   </>
 );
 
